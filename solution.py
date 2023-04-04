4cf63d5a07c823ec329a5786b142a6abda541647
Hunter Raymond <33704440+hitemth@users.noreply.github.com>
2023-04-05 01:59:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 156287560 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    # v = v_0 + at => a = v/10
+    error = 31 + np.exp(1)
+    a = (x + np.random.normal(0, error, size=len(x))) / 10
+
+
+    return a.mean() # Ваш ответ

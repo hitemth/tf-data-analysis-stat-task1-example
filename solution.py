08bcf948b758f01c985c84145cfc70457e0fc4df
Hunter Raymond <33704440+hitemth@users.noreply.github.com>
2023-04-05 04:26:33 +0300

    Naive solution

--- solution.py
+++ solution.py
@@ -22,19 +22,31 @@ def solution(x: np.array) -> float:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     # v = v_0 + at => a = v/10
+    #error_distribution = np.random.exponential(1, size = len(x))
+    #error_distribution = error_distribution - 31
+    a = x.mean()/ 10
+    
+    return a # Ваш ответ
+'''
+def solution_2(x: np.array) -> float:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    # v = v_0 + at => a = v/10
     error_distribution = np.random.exponential(1, size = len(x))
     error_distribution = error_distribution - 31
-    a = (x + error_distribution) / 10
+    a = (x + error_distribution).mean() / 10
     
-    return a.mean() # Ваш ответ
-'''
+    return a # Ваш ответ
+
 from sklearn.metrics import mean_squared_error as mse
 arr = [1,2,3,4,5,6,7,8,9,10] 
 
 sols_1 = []
 sols_2 = []
-for i in range(10):
+for i in range(1):
     ar = np.random.exponential(size = 10)
     sols_1.append(solution(ar))
     sols_2.append(solution_2(ar))
-print(np.mean((np.array(sols_1) - np.array(sols_2))**2))'''
+print(np.mean((np.array(sols_1) - np.array(sols_2))**2))
+'''
